10da89d9e74de9137474e3b4c0adaae280eda006
il_sa <sereg.il@yandex.ru>
2022-06-29 16:31:44 +0300

    new

--- main.py
+++ main.py
@@ -12,11 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_hi('PyCharm')
-    print_hi('Sergei')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-x = 5
-
-y = 12
