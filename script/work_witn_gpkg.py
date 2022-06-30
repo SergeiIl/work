1d4bced9faf8b0e9517d72ea5f5f6d25316594a2
il_sa <sereg.il@yandex.ru>
2022-06-30 19:56:56 +0300

    new

--- script/work_witn_gpkg.py
+++ script/work_witn_gpkg.py
@@ -3,7 +3,22 @@ import matplotlib.pyplot as plt
 import sqlite3 as sq
 import pandas as pd
 
-gpkg = gpd.read_file(r'moscow.gpkg', layer='moscow').head(1000)
-gpkg['created_at'] = pd.to_datetime(gpkg['created_at'])
+# Загрузка данных из gpkg
+gpkg = gpd.read_file(r'moscow.gpkg', layer='moscow')
 
-#gpkg['user_id'].nunique()
+# Изменение формата на формат datatime
+gpkg['created_at'] = pd.to_datetime(gpkg['created_at'])
+# gpkg['week'] = gpkg['created_at'].dt.week
+# gpkg['month'] = gpkg['created_at'].dt.month
+# gpkg['year'] = gpkg['created_at'].dt.year
+
+# Проверка уникальности значений
+nunique = gpkg.nunique()
+
+# Проверка пропущенных значений
+isna = gpkg.isna().sum()
+
+# График совершения событий во времени
+gpkg['created_at'].hist(bins=50, figsize=(16, 6))
+plt.title('Распределение кол-ва событий во времени')
+plt.show()
